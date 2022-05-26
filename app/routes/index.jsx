30e2959dd0955f3306eddce2303d8876be3ac112
Marc A. Maceira Zayas <mmaceira.zayas@gmail.com>
2022-05-26 17:15:22 -0400

    Initialized repository.

---
+++ app/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gradient-to-br from-emerald-300 via-cyan-300 to-purple-300">
+      <div className="flex w-3/4 justify-center">
+        <h1 className="font-mono text-6xl font-bold text-white">
+          Welcome to <br />
+          <span className="font-mono font-extrabold tracking-wider text-gray-800">
+            Remix Tailwind Starter
+          </span>
+        </h1>
+      </div>
+    </div>
+  )
+}
